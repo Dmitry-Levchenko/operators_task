33c19ca16e21e66952fba38ae9d186f5605579e6
Dmitry Levchenko <dmitrik.kiev@gmail.com>
2025-06-23 16:52:52 +0300

    feat(DART-06): ternar operators task

---
+++ 4_task.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+void main(){
+// Завдання 4: Тернарний оператор
+// 1) Створіть змінну temperature.
+// 2) Використайте тернарний оператор, щоб визначити:
+// Якщо більше 25 — “Тепло”.
+// Якщо менше або дорівнює 25 — “Прохолодно”.
+// 3) Потім створіть ще один тернарний оператор:
+// Якщо менше 10 — вивести “Дуже холодно”.
+  stdout.write('Яка температура за вікном? ');
+  String? input = stdin.readLineSync();
+
+  if (input == null || int.tryParse(input) == null) {
+    print('Будь ласка, введіть коректне значення температури.');
+    return;
+  }
+
+  int temperature = int.parse(input);
+
+  String tempDescription = temperature > 25 ? "Тепло" : "Прохолодно";
+  String coldDescription = temperature < 10 ? "Дуже холодно" : tempDescription;
+
+  print('Опис температури: $coldDescription');
+}
+
